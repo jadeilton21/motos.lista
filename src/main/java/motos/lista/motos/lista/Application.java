af821b0bbd4eee65d95bbe8e0705790f957a6acc
Jadeilton <jadeiltondm@gmail.com>
2024-02-24 10:10:08 -0300

    feat: create new project

---
+++ src/main/java/motos/lista/motos/lista/Application.java
@@ -0,0 +1,13 @@
+package motos.lista.motos.lista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/motos/lista/motos/lista/ApplicationTests.java
@@ -0,0 +1,13 @@
+package motos.lista.motos.lista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
